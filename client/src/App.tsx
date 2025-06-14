3c32005b0ddf38d43d93171328349c27953303db
the-ict <@dvltinv@gmail.com>
2025-06-14 11:35:52 +0500

    messages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,11 @@
+// client/src/App.tsx
 import "./main.css"
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import SearchedForm from "./pages/SearchedForm";
-import SingleAccount from "./pages/SingleAccount";
-import Dashboard from "./pages/Dashboard"
 import Messages from "./components/Messages";
-
-
-const require  = true;
-
-
 
 export default function App() {
   return (
     <div>
-      <Messages/>
+      <Messages />
     </div>
-  )
+  );
 }
