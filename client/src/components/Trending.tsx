45015e0ecb4db2752f109b5088bcc08878a0bed4
the-ict <@dvltinv@gmail.com>
2025-06-17 08:44:59 +0500

    trending changed

--- client/src/components/Trending.tsx
+++ client/src/components/Trending.tsx
@@ -1,3 +1,9 @@
+import pubgGameIcon from "@/assets/pubg1.png";
+import codGameIcon from "@/assets/callofduty.png"
+import csgoGameIcon from "@/assets/fortnite.png";
+import React from "react";
+
+
 export default function Trending() {
     return (
         <div className="flex items-center bg-[#000000da] justify-center pt-10 text-white py-20">
@@ -5,37 +11,67 @@ export default function Trending() {
                 <h1 className="text-3xl font-bold">Eng qimmat akkountlar</h1>
                 <div className="flex items-center gap-10 flex-wrap mt-10 ">
                     <div className="w-[270px] relative border">
-                        <img className="w-full h-full object-contain cursor-pointer hover:blur-[5px] transition-all duration-300 ease-in-out"  src="https://www.g2g.com/img/affiliate-home.webp" alt="" />
-                        <p className="absolute bottom-0 left-0 right-0 text-center w-full bg-gray-200 text-black py-3">narxi: 100$</p>
+                        <img className="w-full h-full object-contain cursor-pointer hover:blur-[5px] transition-all duration-300 ease-in-out" src="https://www.g2g.com/img/affiliate-home.webp" alt="" />
+                        <div className="absolute bottom-0 left-0 right-0 text-center w-full bg-gray-200 text-black py-3 flex items-center justify-between px-3">
+                            <p>narxi: 100$</p>
+                            <img src={pubgGameIcon} className="w-[30px] h-[30px] object-cover cursor-pointer" alt="pubg game icon" />
+                        </div>
                     </div>
+                    
                     <div className="w-[270px] relative border">
-                        <img className="w-full h-full object-contain cursor-pointer hover:blur-[5px] transition-all duration-300 ease-in-out"  src="https://www.g2g.com/img/affiliate-home.webp" alt="" />
-                        <p className="absolute bottom-0 left-0 right-0 text-center w-full bg-gray-200 text-black py-3">narxi: 100$</p>
-                    </div>
-                    <div className="w-[270px] relative border">
-                        <img className="w-full h-full object-contain cursor-pointer hover:blur-[5px] transition-all duration-300 ease-in-out"  src="https://www.g2g.com/img/affiliate-home.webp" alt="" />
-                        <p className="absolute bottom-0 left-0 right-0 text-center w-full bg-gray-200 text-black py-3">narxi: 100$</p>
-                    </div>
-                    <div className="w-[270px] relative border">
-                        <img className="w-full h-full object-contain cursor-pointer hover:blur-[5px] transition-all duration-300 ease-in-out"  src="https://www.g2g.com/img/affiliate-home.webp" alt="" />
-                        <p className="absolute bottom-0 left-0 right-0 text-center w-full bg-gray-200 text-black py-3">narxi: 100$</p>
+                        <img className="w-full h-full object-contain cursor-pointer hover:blur-[5px] transition-all duration-300 ease-in-out" src="https://www.g2g.com/img/affiliate-home.webp" alt="" />
+                        <div className="absolute bottom-0 left-0 right-0 text-center w-full bg-gray-200 text-black py-3 flex items-center justify-between px-3">
+                            <p>narxi: 100$</p>
+                            <img src={pubgGameIcon} className="w-[30px] h-[30px] object-cover cursor-pointer" alt="pubg game icon" />
+                        </div>
                     </div>
 
                     <div className="w-[270px] relative border">
-                        <img className="w-full h-full object-contain cursor-pointer hover:blur-[5px] transition-all duration-300 ease-in-out"  src="https://www.g2g.com/img/affiliate-home.webp" alt="" />
-                        <p className="absolute bottom-0 left-0 right-0 text-center w-full bg-gray-200 text-black py-3">narxi: 100$</p>
+                        <img className="w-full h-full object-contain cursor-pointer hover:blur-[5px] transition-all duration-300 ease-in-out" src="https://www.g2g.com/img/affiliate-home.webp" alt="" />
+                        <div className="absolute bottom-0 left-0 right-0 text-center w-full bg-gray-200 text-black py-3 flex items-center justify-between px-3">
+                            <p>narxi: 100$</p>
+                            <img src={pubgGameIcon} className="w-[30px] h-[30px] object-cover cursor-pointer" alt="pubg game icon" />
+                        </div>
                     </div>
+
                     <div className="w-[270px] relative border">
-                        <img className="w-full h-full object-contain cursor-pointer hover:blur-[5px] transition-all duration-300 ease-in-out"  src="https://www.g2g.com/img/affiliate-home.webp" alt="" />
-                        <p className="absolute bottom-0 left-0 right-0 text-center w-full bg-gray-200 text-black py-3">narxi: 100$</p>
+                        <img className="w-full h-full object-contain cursor-pointer hover:blur-[5px] transition-all duration-300 ease-in-out" src="https://www.g2g.com/img/affiliate-home.webp" alt="" />
+                        <div className="absolute bottom-0 left-0 right-0 text-center w-full bg-gray-200 text-black py-3 flex items-center justify-between px-3">
+                            <p>narxi: 100$</p>
+                            <img src={pubgGameIcon} className="w-[30px] h-[30px] object-cover cursor-pointer" alt="pubg game icon" />
+                        </div>
                     </div>
+
                     <div className="w-[270px] relative border">
-                        <img className="w-full h-full object-contain cursor-pointer hover:blur-[5px] transition-all duration-300 ease-in-out"  src="https://www.g2g.com/img/affiliate-home.webp" alt="" />
-                        <p className="absolute bottom-0 left-0 right-0 text-center w-full bg-gray-200 text-black py-3">narxi: 100$</p>
+                        <img className="w-full h-full object-contain cursor-pointer hover:blur-[5px] transition-all duration-300 ease-in-out" src="https://www.g2g.com/img/affiliate-home.webp" alt="" />
+                        <div className="absolute bottom-0 left-0 right-0 text-center w-full bg-gray-200 text-black py-3 flex items-center justify-between px-3">
+                            <p>narxi: 100$</p>
+                            <img src={pubgGameIcon} className="w-[30px] h-[30px] object-cover cursor-pointer" alt="pubg game icon" />
+                        </div>
                     </div>
+
                     <div className="w-[270px] relative border">
-                        <img className="w-full h-full object-contain cursor-pointer hover:blur-[5px] transition-all duration-300 ease-in-out"  src="https://www.g2g.com/img/affiliate-home.webp" alt="" />
-                        <p className="absolute bottom-0 left-0 right-0 text-center w-full bg-gray-200 text-black py-3">narxi: 100$</p>
+                        <img className="w-full h-full object-contain cursor-pointer hover:blur-[5px] transition-all duration-300 ease-in-out" src="https://www.g2g.com/img/affiliate-home.webp" alt="" />
+                        <div className="absolute bottom-0 left-0 right-0 text-center w-full bg-gray-200 text-black py-3 flex items-center justify-between px-3">
+                            <p>narxi: 100$</p>
+                            <img src={pubgGameIcon} className="w-[30px] h-[30px] object-cover cursor-pointer" alt="pubg game icon" />
+                        </div>
+                    </div>
+
+                    <div className="w-[270px] relative border">
+                        <img className="w-full h-full object-contain cursor-pointer hover:blur-[5px] transition-all duration-300 ease-in-out" src="https://www.g2g.com/img/affiliate-home.webp" alt="" />
+                        <div className="absolute bottom-0 left-0 right-0 text-center w-full bg-gray-200 text-black py-3 flex items-center justify-between px-3">
+                            <p>narxi: 100$</p>
+                            <img src={pubgGameIcon} className="w-[30px] h-[30px] object-cover cursor-pointer" alt="pubg game icon" />
+                        </div>
+                    </div>
+
+                    <div className="w-[270px] relative border">
+                        <img className="w-full h-full object-contain cursor-pointer hover:blur-[5px] transition-all duration-300 ease-in-out" src="https://www.g2g.com/img/affiliate-home.webp" alt="" />
+                        <div className="absolute bottom-0 left-0 right-0 text-center w-full bg-gray-200 text-black py-3 flex items-center justify-between px-3">
+                            <p>narxi: 100$</p>
+                            <img src={pubgGameIcon} className="w-[30px] h-[30px] object-cover cursor-pointer" alt="pubg game icon" />
+                        </div>
                     </div>
                 </div>
             </div>
