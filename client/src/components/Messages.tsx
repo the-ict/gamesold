e0a0c62dc0ab286edc60f0ea342406cf023d90d2
the-ict <dvltinv@gmail.com>
2025-07-12 16:47:13 +0500

    keydown

--- client/src/components/Messages.tsx
+++ client/src/components/Messages.tsx
@@ -200,82 +200,101 @@ export default function Messages({}: Props) {
         </div>
 
         <div className="w-[70%] rounded-lg bg-[#2A2A2A] h-full overflow-hidden flex flex-col justify-between">
-          <div className="h-[10%] flex items-center justify-between p-5 border-b border-gray-600">
-            <div className="flex items-center gap-4">
-              <img
-                src={
-                  receiver.name && receiver?.image.includes("google")
-                    ? receiver.image
-                    : receiver.image
-                    ? IMAGE_URL + receiver.image
-                    : userAvatar
-                }
-                alt="User default avatar"
-                className="w-[50px] h-[50px] object-cover cursor-pointer rounded-full"
-              />
-              <span className="font-bold cursor-pointer">{receiver.name}</span>
+          {currentConversation._id && (
+            <div className="h-[10%] flex items-center justify-between p-5 border-b border-gray-600">
+              <div className="flex items-center gap-4">
+                <img
+                  src={
+                    receiver.name && receiver?.image.includes("google")
+                      ? receiver.image
+                      : receiver.image
+                      ? IMAGE_URL + receiver.image
+                      : userAvatar
+                  }
+                  alt="User default avatar"
+                  className="w-[50px] h-[50px] object-cover cursor-pointer rounded-full"
+                />
+                <span className="font-bold cursor-pointer">
+                  {receiver.name}
+                </span>
+              </div>
+
+              <PiDotsNine className="text-2xl text-gray-400 cursor-pointer hover:text-white transition-colors" />
             </div>
+          )}
 
-            <PiDotsNine className="text-2xl text-gray-400 cursor-pointer hover:text-white transition-colors" />
-          </div>
+          {currentConversation._id ? (
+            <div className="h-[80%]">
+              <div className="flex flex-col gap-4 p-5 overflow-y-auto h-full no-scrollbar">
+                {/* Example messages */}
+                {Array.isArray(chatMessages) ? (
+                  chatMessages.map((message) => (
+                    <div ref={messageRef}>
+                      <MessagesItem
+                        key={message._id}
+                        message={message}
+                        own={message.sender === userId}
+                      />
+                    </div>
+                  ))
+                ) : (
+                  <div className="bg-blue-500 text-white p-3 rounded-lg self-end max-w-[70%]">
+                    <p>Xabarlar mavjud emas</p>
+                  </div>
+                )}
+              </div>
+            </div>
+          ) : (
+            <div className="h-[100%] flex items-center justify-center">
+              <p className="text-gray-400">Biror chatni tanlang!</p>
+            </div>
+          )}
 
-          <div className="h-[80%]">
-            <div className="flex flex-col gap-4 p-5 overflow-y-auto h-full no-scrollbar">
-              {/* Example messages */}
-              {Array.isArray(chatMessages) ? (
-                chatMessages.map((message) => (
-                  <div ref={messageRef}>
-                    <MessagesItem
-                      key={message._id}
-                      message={message}
-                      own={message.sender === userId}
+          {currentConversation._id && (
+            <div className="h-[10%] flex items-center justify-between p-5 border-t border-gray-600">
+              <input
+                type="text"
+                value={messages}
+                onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                  setMessages(e.target.value);
+                }}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    handleSubmit();
+                  }
+                }}
+                className="flex-1 bg-transparent border-none outline-none text-white placeholder:text-gray-400"
+                placeholder="Xabaringizni shu yerda yozing..."
+              />
+              <div className="relative flex items-center gap-2 ml-5">
+                <button
+                  onClick={handleSubmit}
+                  className="bg-blue-500 cursor-pointer text-white px-4 py-2 rounded-lg"
+                >
+                  Jo'natish
+                </button>
+                <button
+                  className="bg-gray-600 text-white px-4 py-2 rounded-lg cursor-pointer"
+                  onClick={() => setShowEmojiPicker(!showEmojiPicker)}
+                >
+                  {showEmojiPicker ? "X" : "Emoji"}
+                </button>
+                {showEmojiPicker && (
+                  <div className="absolute bottom-12 right-0 z-10">
+                    <EmojiPicker
+                      onEmojiClick={(emoji) => {
+                        setMessages((previous) => previous + emoji.emoji);
+                        setShowEmojiPicker(false);
+                      }}
+                      width={300}
+                      height={400}
+                      previewConfig={{ showPreview: false }}
                     />
                   </div>
-                ))
-              ) : (
-                <p>NO MESSAGES</p>
-              )}
+                )}
+              </div>
             </div>
-          </div>
-
-          <div className="h-[10%] flex items-center justify-between p-5 border-t border-gray-600">
-            <input
-              type="text"
-              value={messages}
-              onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                setMessages(e.target.value)
-              }
-              className="flex-1 bg-transparent border-none outline-none text-white placeholder:text-gray-400"
-              placeholder="Type your message here..."
-            />
-            <div className="relative flex items-center gap-2 ml-5">
-              <button
-                onClick={handleSubmit}
-                className="bg-blue-500 cursor-pointer text-white px-4 py-2 rounded-lg"
-              >
-                Send
-              </button>
-              <button
-                className="bg-gray-600 text-white px-4 py-2 rounded-lg cursor-pointer"
-                onClick={() => setShowEmojiPicker(!showEmojiPicker)}
-              >
-                {showEmojiPicker ? "X" : "Emoji"}
-              </button>
-              {showEmojiPicker && (
-                <div className="absolute bottom-12 right-0 z-10">
-                  <EmojiPicker
-                    onEmojiClick={(emoji) => {
-                      setMessages((previous) => previous + emoji.emoji);
-                      setShowEmojiPicker(false);
-                    }}
-                    width={300}
-                    height={400}
-                    previewConfig={{ showPreview: false }}
-                  />
-                </div>
-              )}
-            </div>
-          </div>
+          )}
         </div>
       </div>
     </div>
